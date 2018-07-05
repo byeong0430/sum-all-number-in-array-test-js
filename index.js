f5a167d01f8ff9976ac75a76191dc348129d5d54
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-05 00:08:13 -0700

    solved recursion problem

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function sumItems(array) {
+  let sum = 0;
+
   // Sum all the numbers in the array
+  // 1. loop through array
+  for (item of array) {
+    if (Array.isArray(item)) {
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  }
+  return sum;
 }
 
+console.log(sumItems([1, 2, 3, 4, 5]));
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
 module.exports = sumItems;
